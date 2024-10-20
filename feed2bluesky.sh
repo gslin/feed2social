5610e2fccd7efe0a1a65070b012837472d466e52
Gea-Suan Lin <gslin@gslin.org>
2024-10-20 17:48:05 +0800

    feat: use a shell script to wrap python scripts.

---
+++ feed2bluesky.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Support mise & pyenv & native environment:
+if command -v mise; then
+    eval "$(mise activate bash)"
+elif [[ -d "${HOME}/.pyenv"]]; then
+    export PATH="${HOME}/.pyenv/shims:${HOME}/.pyenv/bin:${PATH}"
+    eval "$(pyenv init -)"
+fi
+
+BASEDIR="$(dirname $0)"
+"${BASEDIR}/feed2bluesky.py" || true
