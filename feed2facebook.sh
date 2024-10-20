8c147694a2676286f4aa97f0c8ddad9ebd3ef7fe
Gea-Suan Lin <gslin@gslin.org>
2024-10-20 17:49:13 +0800

    feat: support mise & pyenv & native environment for python scripts.

--- feed2facebook.sh
+++ feed2facebook.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-LANG=en_US.UTF-8 \
-    ~/.pyenv/shims/python3 ~/git/feed2social/feed2facebook.py || \
-    true
+#
+# Support mise & pyenv & native environment:
+if command -v mise; then
+    eval "$(mise activate bash)"
+elif [[ -d "${HOME}/.pyenv"]]; then
+    export PATH="${HOME}/.pyenv/shims:${HOME}/.pyenv/bin:${PATH}"
+    eval "$(pyenv init -)"
+fi
+
+BASEDIR="$(dirname $0)"
+LANG=en_US.UTF-8 "${BASEDIR}/feed2facebook.py" || true
 
 # Since our pkill run with parent pid filtering (ppid == 1), we need to
 # kill geckodriver first, then firefox-esr afterwards.
