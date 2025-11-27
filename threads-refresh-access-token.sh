fcae7bd6089403fa585c7da3f188a3822f11fe37
Gea-Suan Lin <gslin@gslin.org>
2025-11-28 01:45:58 +0800

    feat: add a script to load python environment.

---
+++ threads-refresh-access-token.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Support mise & pyenv & native environment:
+if command -v mise > /dev/null; then
+    eval "$(mise activate bash --shims)"
+elif [[ -d "${HOME}/.pyenv" ]]; then
+    export PATH="${HOME}/.pyenv/shims:${HOME}/.pyenv/bin:${PATH}"
+    eval "$(pyenv init -)"
+fi
+
+cd "$(dirname $0)"
+LANG=en_US.UTF-8 ./threads-refresh-access-token.py || true
