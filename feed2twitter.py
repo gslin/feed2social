421acabc8fa9604dfd42b45435da826498ea0ed0
Gea-Suan Lin <gslin@gslin.org>
2025-11-29 03:39:45 +0800

    fix: add missing application/json header.

--- feed2twitter.py
+++ feed2twitter.py
@@ -115,6 +115,7 @@ class Feed2Twitter(object):
                 res = httpx.post(
                     'https://api.twitter.com/2/tweets',
                     auth=auth,
+                    headers={'Content-Type': 'application/json'},
                     json={'text': content},
                 )
                 print('* res = {}'.format(res))
@@ -131,6 +132,7 @@ class Feed2Twitter(object):
                 res = httpx.post(
                     'https://api.twitter.com/2/tweets',
                     auth=auth,
+                    headers={'Content-Type': 'application/json'},
                     json={
                         'text': f'Sync from: {url}',
                         'reply': {'in_reply_to_tweet_id': tweet_id},
